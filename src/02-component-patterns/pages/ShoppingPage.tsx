bf6131bc6db52dd365443cbc48ba97bfb30cc906
Francisco Huapaya <franciscojho@hotmail.com>
2021-12-30 15:48:24 -0500

    Component compound ready

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { ProductCart } from '../components/ProductCart'
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components'
+
+const product = {
+    id: '1',
+    title: 'Coffe mug - Card',
+    img: './coffee-mug.png'
+}
 
 export const ShoppingPage = () => {
     return (
         <div>
             <h1>Shopping Page</h1>
             <hr />
-            <ProductCart />
-        </div>
-    )
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap'
+            }}>
+                <ProductCard product={ product }>   
+                    <ProductCard.Image />
+                    <ProductCard.Title />
+                    <ProductCard.Buttons />
+                </ProductCard>
+                <ProductCard product={ product }>   
+                    <ProductImage />
+                    <ProductTitle title={'Cafe'} />
+                    <ProductButtons />
+                </ProductCard>
+            </div>
+        </div>  
+    )   
 }
